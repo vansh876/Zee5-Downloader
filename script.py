1e07da80ac82ab3d1f1ec6834956573d39217f7a
Redop1189 <77840748+Redop1189@users.noreply.github.com>
2021-03-23 10:39:50 +0530

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ I'm A simple Zee5 link downloader bot With Permanent Thumbnail Support💯.
 
 Please send me any Zee5 link, I can upload it to telegram as File/Video.
 
-Currently I'm in beta mode 🥺, If found any bugs, report @TroJanzSupport !
+Currently I'm in beta mode 🥺, If found any bugs, report @movietrigger !
 
 Click <i>/help</i> for more details....</b>"""
 
@@ -21,15 +21,15 @@ Click <i>/help</i> for more details....</b>"""
 NOTE: Download may take some time! So please wait for it to complete!"""
 
 
-    ABOUT_TEXT = """⭕️<b>My Name : TroJanZ Zee5 DL</b>
+    ABOUT_TEXT = """⭕️<b>My Name : Movietrigger Zee5 DL</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @movietrigger
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
-⭕️<b>Source Code :</b> 👉 <a href='https://github.com/TroJanzHEX/Zee5-Downloader'>Click Here</a>"""
+⭕️<b>Source Code :</b> 👉 <a href='https://github.com/Redop1189/Zee5-Downloader'>Click Here</a>"""
 
 
 
